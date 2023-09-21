b063a3d6fc0ab3f39b2f9d0a80c87084905d660e
Charly <icharlieanne@gmail.com>
2023-09-21 21:31:26 +0100

    finishing touch on styling

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,6 +1,3 @@
-// import Header from '../components/Header';
-import Image from '../components/Image';
-import { ImageData } from '../components/ImageData';
 import Loader from '../components/Loader';
 import Header from '../components/Header';
 import { useEffect, useState } from 'react';
@@ -20,11 +17,7 @@ function Library() {
       {isLoading ? (
         <Loader />
       ) : (
-        <section className="grid grid-cols-1 place-items-center gap-6 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {ImageData.map((image) => (
-            <Image image={image} key={image.id} />
-          ))}
-        </section>
+        <h1 className="large">We miss you already. Kindly log back in!</h1>
       )}
     </section>
   );
